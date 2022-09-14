3b24d5df32a09ef8891a14a1812eef918c1fe234
Jussi Vatjus-Anttila <jussi.vatjus-anttila@ouraring.com>
2022-09-14 13:03:57 +0300

    preparing client fixture

--- pytest_stf/plugin.py
+++ pytest_stf/plugin.py
@@ -29,7 +29,7 @@ def pytest_addoption(parser):
     )
     group.addoption(
         "--stf_allocation_timeout",
-        default=10,
+        default=1000,
         help="Allocation timeout",
     )
 
@@ -58,11 +58,28 @@ def pytest_unconfigure(config):
         del config._openstf  # pylint: disable=protected-access
 
 
-@pytest.fixture(name="selected_phone", scope="session")
-def fixture_selected_phone(pytestconfig):
+@pytest.fixture(name='allocated_phone', scope="session")
+def fixture_allocated_phone(pytestconfig):
     """
-    Allocate required phone, create ADB tunnel to phone via STF and start appium server for tests.
-    Yields tuple (device: dict, adb: AdbServer, appium: Appium)
+    Allocate required phone via STF.
+    Yields device details as dict.
+    e.g.
+    {
+        "abi": "arm64-v8a",
+        "airplaneMode": false,
+        "battery": {...},
+        "browser": {...},
+        "cpuPlatform": "...",
+        "manufacturer": "...",
+        "marketName": "Pixel 4",
+        "model": "Pixel 4",
+        "network": {...},
+        "platform": "Android",
+        "sdk": "32",
+        "serial": "...",
+        "version": "12",
+        ...
+    }
     """
     assert hasattr(pytestconfig, '_openstf'), 'stf_host or stf_token missing'
 
@@ -74,6 +91,48 @@ def fixture_selected_phone(pytestconfig):
     requirements = parse_requirements(requirements)
 
     with stf.allocation_context(requirements, timeout_seconds=timeout) as device:
-        with AdbServer(device['remote_adb_url']) as adb:
-            with Appium() as appium:
-                yield device, adb, appium
+        yield device
+
+
+@pytest.fixture(name='phone_with_adb', scope="session")
+def fixture_phone_with_adb(allocated_phone):
+    """
+    Allocate required phone and create ADB tunnel to phone via STF.
+    Yields tuple (adb: AdbServer, device: dict)
+    """
+    with AdbServer(allocated_phone['remote_adb_url']) as adb:
+        yield allocated_phone, adb
+
+
+@pytest.fixture(name="appium_server", scope="session")
+def fixture_appium_server(phone_with_adb):
+    """
+    Allocate required phone, create ADB tunnel to phone via STF and start appium server for tests.
+    Yields tuple (appium: Appium, adb: AdbServer, device: dict )
+    """
+    phone, adb = phone_with_adb
+    with Appium() as appium:
+        yield appium, adb, phone
+
+
+@pytest.fixture(name='capabilities')
+def fixture_capabilities(pytestconfig, allocated_phone):
+    capabilities = {
+        "platformName": allocated_phone['platform'],
+        'udid': allocated_phone['serial'],
+    }
+    yield capabilities
+
+@pytest.fixture(name="appium_client", scope="session")
+def fixture_appium_client(appium_server, capabilities):
+    yield appium, adb, phone
+
+
+@pytest.fixture(name="selected_phone", scope="session")
+def fixture_selected_phone(appium_server):
+    """
+    Alternative for appium_server -fixture except tuple is in different order:
+    (device, adb, appium)
+    """
+    appium, adb, device = appium_server
+    yield device, adb, appium
--- sample/test_samples.py
+++ sample/test_samples.py
@@ -1,4 +1,28 @@
-def test_create(selected_phone):
-    device, adb, appium = selected_phone
-    print(device)
+from time import sleep
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+
+def test_adb(phone_with_adb):
+    phone, adb = phone_with_adb
+    print(phone)
+    sleep(100)
+
+
+def test_appium(appium_server):
+    appium, adb, phone = appium_server
+    print(phone)
+    #print(adb._execute('devices'))
     print(f'wd_hub: {appium.get_wd_hub_uri()}')
+
+
+    options = UiAutomator2Options()
+    options.platform_name = phone['platform']
+    options.platform_Version = phone['version']
+    #options.udid = phone['serial']
+    options.set_capability("browser_name", "Browser")
+    driver = webdriver.Remote(appium.get_wd_hub_uri(), options=options)
+
+    print(driver)
+
+    driver.quit()
+
